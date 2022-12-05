ff43f2e7ac1dc92d2f3b979fa7fbccdca173fbfc
Guustaa <gustavopereira742@gmail.com>
2022-12-05 02:35:05 -0300

    adição das funções

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.querySelector("#app").innerHTML = `
 
-<header>
+      <header>
             <img src="./assets/logo.svg" alt="Logo da NLW">
         </header>
         <main id="card">
@@ -17,3 +17,23 @@ document.querySelector("#app").innerHTML = `
         </main>
 
 `
+function createGame(player1, hour, player2) {
+  return `
+    <li>
+      <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
+      <strong>${hour}</strong>
+      <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" />
+    </li>
+  `
+}
+
+function createCard(date, day, games) {
+  return `
+    <div class="card">
+      <h2>${date} <span>${day}</span></h2>
+      <ul>
+        ${games}
+      </ul>
+    </div>
+  `
+}
