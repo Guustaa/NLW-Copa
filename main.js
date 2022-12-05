df85266ee18e450852c1477fe94f514a75fb5bda
Guustaa <gustavopereira742@gmail.com>
2022-12-05 02:58:55 -0300

    colocando os icones das bandeiras

--- main.js
+++ main.js
@@ -1,28 +1,24 @@
 document.querySelector("#app").innerHTML = `
-
-      <header>
-            <img src="./assets/logo.svg" alt="Logo da NLW">
-        </header>
-        <main id="card">
-            <div class="card">
-                <h2>24/11 <span>quinta</span></h2>
-                <ul>
-                    <li>
-                        <img src="./assets/icon brazil.svg" alt="Bandeira do Brasil">
-                        <strong>16:00</strong>
-                        <img src="./assets/icon serbia.svg" alt="Bandeira da Sérvia">
-                    </li>
-                </ul>
-            </div>
-        </main>
-
+  <header>
+    <img src="./assets/logo.svg" alt="Logo da NLW" />
+  </header>
+  <main id="cards">
+    ${createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia"))}
+    ${createCard(
+      "28/11",
+      "segunda",
+      createGame("switzerland", "13:00", "brazil") +
+        createGame("portugal", "16:00", "uruguai")
+    )}
+    ${createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))}
+  </main>
 `
 function createGame(player1, hour, player2) {
   return `
     <li>
-      <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
+      <img src="./assets/icon ${player1}.svg" alt="Bandeira do ${player1}" />
       <strong>${hour}</strong>
-      <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" />
+      <img src="./assets/icon ${player2}.svg" alt="Bandeira da ${player2}" />
     </li>
   `
 }
